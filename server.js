5fad1e41f3f335e539d5e4b2a8f907131bd492bf
David <davidjohnsonny@gmail.com>
2024-05-07 22:38:43 -0700

    Added Express server

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express');
 
 // Creating an Express application
 const app = express();
+const path = require('path');
 
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 // Start the server
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port} http://localhost:3000/`);
 });
+
+app.use(express.static("build"));
