503f366853c62ff8e96f5e9f00f628ac51b0c571
David <davidjohnsonny@gmail.com>
2024-05-14 03:42:59 -0700

    modified the correct path to scp transfer files

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* /Home/Psyko/DavidJNy2_1/server
+scp -r build/ /home/Psyko/DavidJNy2_1/server
 
 #serve -s build -l 3000
 cd server
