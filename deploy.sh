7e600ef7d628c4e31b0baf398f47bbf13528aac0
David <davidjohnsonny@gmail.com>
2024-05-15 03:39:16 -0700

    added filepath

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 echo "Switching to branch master"
-git checkout main
+echo "Pulling from main"
+
+sudo git pull
 
 echo "Building app..."
+
 npm run build
 
 echo "Deploying files to server..."
 scp -r build/ /home/Psyko/DavidJNy2_1/server
 
 #serve -s build -l 3000
-cd server
-npm run start
+file_path="/home/Psyko/git/DavidJNy2_1/server/server.js"
+
+node "$file_path"
 
 echo "Done!"
 
