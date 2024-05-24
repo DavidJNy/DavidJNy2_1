04fc9fdc517170dbbccd0ff5654342e06c200f73
David <davidjohnsonny@gmail.com>
2024-05-24 14:47:23 -0700

    added websocket server to bash cmd

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,15 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/ /home/git/DavidJNy2_1/server
+sudo scp -r build/ /home/git/DavidJNy2_1/server
 
 #serve -s build -l 3000
 file_path="/home/git/DavidJNy2_1/server/server.js"
+file_path_websocket="/home/git/DavidJNy2_1/server/websocket.js"
 
 node "$file_path"
+node "$file_path_websocket"
+
 
 echo "Done!"
 
