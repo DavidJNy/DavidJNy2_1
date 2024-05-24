53a1d2f5deb93a1f25000868b261a430afda936e
David <davidjohnsonny@gmail.com>
2024-05-24 15:15:46 -0700

    adjusting the bash commands

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
+#serve -s build -l 3000
+server_path="/home/git/DavidJNy2_1/server/"
+file_path="/home/git/DavidJNy2_1/server/server.js"
+file_path_websocket="/home/git/DavidJNy2_1/server/websocket.js"
+
+
 echo "Switching to branch master"
 echo "Pulling from main"
 
 sudo git pull
+
+npm install
+
+cd /server
+npm install
+cd ../DavidJNy2_1
 
 echo "Building app..."
 
@@ -10,12 +22,18 @@ npm run build
 echo "Deploying files to server..."
 sudo scp -r build/ /home/git/DavidJNy2_1/server
 
-#serve -s build -l 3000
-file_path="/home/git/DavidJNy2_1/server/server.js"
-file_path_websocket="/home/git/DavidJNy2_1/server/websocket.js"
+#kill server
+echo "Stopping server"
+sudo systemctl stop myexpressapp
 
+echo "Loading new server"
 node "$file_path"
 node "$file_path_websocket"
+
+
+echo "Rebooting server/services"
+sudo systemctl start myexpressapp
+sudo systemctl restart myexpressapp
 
 
 echo "Done!"
