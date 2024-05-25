7fada32a6d29ea51ee88d1d9460e79a5e6a3d028
David <davidjohnsonny@gmail.com>
2024-05-24 22:16:31 -0700

    change socket io target on client side

--- src/components/ChatWebSocket.js
+++ src/components/ChatWebSocket.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 // import './App.css';
 
-const socket = io('http://localhost:3001');
+const socket = io('/ws/');
 
 function ChatWebSocket() {
   const [messages, setMessages] = useState([]);
