80f9da03f626a6a238be3d73513626137b4cdef4
David Ny <davidjohnsonny@gmail.com>
2024-06-24 01:05:40 -0700

    mispelled registerform

--- src/components/parkbuddy/Login.js
+++ src/components/parkbuddy/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Button, Modal } from "react-bootstrap";
-import RegisterForm from "./RegisterForm.js";
+import RegisterForm from "./registerform";
 
 const LoginForm = ({ onLogin }) => {
   const [username, setUsername] = useState("");
